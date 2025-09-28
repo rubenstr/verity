a6526c5f766ebd1fdf9c475bbec570ab7012ac5c
rubenstr <rubens.teixeira_terceiros@nhecotech.com>
2025-09-28 19:33:51 -0400

    bootstrap installation

--- customer-registration/src/app/app.component.ts
+++ customer-registration/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { HomeRegistrationComponent } from './home/home-registration/home-registration.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet]
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet, HomeRegistrationComponent]
 })
 export class AppComponent {
   title = 'customer-registration';
