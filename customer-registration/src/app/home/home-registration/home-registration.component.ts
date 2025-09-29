e35f67edb4b1bd251ca01f2ec6d639ad091ad9cc
rubenstr <rubens.teixeira_terceiros@nhecotech.com>
2025-09-28 20:49:27 -0400

    add simple header

--- customer-registration/src/app/home/home-registration/home-registration.component.ts
+++ customer-registration/src/app/home/home-registration/home-registration.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home-registration',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './home-registration.component.html',
   styleUrls: ['./home-registration.component.scss']
 })
